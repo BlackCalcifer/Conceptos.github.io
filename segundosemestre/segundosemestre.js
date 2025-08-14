ca5e147e4a8d806fbeea9b84a699c230792580e7
BlackCalcifer <evangeliondayztest@gmail.com>
2025-08-13 21:51:03 -0500

    Delete segundosemestre directory

--- segundosemestre/segundosemestre.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("btnPDF").addEventListener("click", function() {
-        const contenido = document.getElementById("contenido"); // Selecciona el contenido a convertir en PDF
-
-        html2pdf()
-            .from(contenido)
-            .save("documento.pdf"); // Guarda el archivo como "documento.pdf"
-    });
-});
